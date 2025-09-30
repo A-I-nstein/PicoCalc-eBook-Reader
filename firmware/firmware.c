5af24f33815fdcbb63a5c8cfbaa7fbf5f0727f95
A-I-nstein <anuranjan.m.b@gmail.com>
2025-09-30 12:35:13 +0530

    pagination logic

--- firmware/firmware.c
+++ firmware/firmware.c
@@ -3,11 +3,16 @@
 
 #include "pico/stdlib.h"
 
+#include "drivers/picocalc.h"
 #include "drivers/fat32.h"
 
 typedef struct header_block header_block;
 
 const size_t HEADER_BLOCK_SIZE = 16;
+const size_t SCREEN_SIZE_X = 40;
+const size_t SCREEN_SIZE_Y = 32;
+const size_t SCREEN_AREA = SCREEN_SIZE_X * SCREEN_SIZE_Y;
+const bool user_interrupt = false;
 
 struct header_block
 {
@@ -38,20 +43,16 @@ void extract_header_block(header_block *block, const uint8_t *bytes)
     block->offset = be_bytes_to_uint32(&bytes[4]);
     block->size = be_bytes_to_uint32(&bytes[8]);
 
-    printf("%u %u %u %u\n", block->data_type, block->section_id, block->offset, block->size);
+    // printf("%u %u %u %u\n", block->data_type, block->section_id, block->offset, block->size);
 }
 
 int main()
 {
     stdio_init_all();
+    picocalc_init();
     sleep_ms(5000);
 
-    printf("Initialising FAT32\n");
-    fat32_init();
-    printf("Initialisation completed\n");
-
     fat32_file_t file;
-    uint32_t current_position = 0;
     fat32_open(&file, "The House of the Dead.book");
 
     size_t bytes_read;
@@ -66,27 +67,87 @@ int main()
         extract_header_block(&block_1, &read_buffer[0]);
         no_of_header_blocks = block_1.offset / HEADER_BLOCK_SIZE;
     }
-    printf("No. of blocks: %u\n", no_of_header_blocks);
+    // printf("No. of blocks: %u\n", no_of_header_blocks);
+
+    fat32_seek(&file, 0);
 
     header_block header[no_of_header_blocks];
 
     for (size_t x = 0; x < no_of_header_blocks; x++)
     {
+        header_block block;
+        result = fat32_read(&file, read_buffer, 16, &bytes_read);
+
+        if (result == FAT32_OK && bytes_read > 0)
+        {
+            extract_header_block(&block, &read_buffer[0]);
+            header[x] = block;
+        }
     }
 
-    // uint32_t section_start_pos = be_bytes_to_uint32(&read_buffer[4]);
-    // uint32_t section_size = be_bytes_to_uint32(&read_buffer[8]);
+    for (size_t x = 0; x < no_of_header_blocks; x++)
+    {
+        if (header[x].size <= SCREEN_AREA)
+        {
+            fat32_seek(&file, header[x].offset);
 
-    // printf("Section starts at: %u\n", section_start_pos);
-    // printf("Section size: %u\n", section_size);
+            size_t bytes_read;
+            char read_buffer[header[x].size + 1];
+            fat32_error_t result = fat32_read(&file, read_buffer, header[x].size, &bytes_read);
 
-    // char read_buffer_2[section_size];
-    // size_t bytes_read_2;
+            if (result == FAT32_OK && bytes_read > 0)
+            {
+                read_buffer[bytes_read] = '\0';
+                printf("\033[2J\033[H");
+                printf("%s", read_buffer);
+                sleep_ms(5000);
+            }
+        }
+        else
+        {
+            uint32_t current_char_index = 0;
+            uint32_t remaining_chars_len = 0;
+            while (current_char_index < header[x].size)
+            {
+                remaining_chars_len = header[x].size - current_char_index;
 
-    // fat32_seek(&file, section_start_pos);
-    // fat32_error_t result_2 = fat32_read(&file, read_buffer_2, section_size, &bytes_read_2);
+                if (remaining_chars_len <= SCREEN_AREA)
+                {
+                    fat32_seek(&file, header[x].offset + current_char_index);
 
-    // printf("Section: %s", read_buffer_2);
+                    size_t bytes_read;
+                    char read_buffer[remaining_chars_len + 1];
+                    fat32_error_t result = fat32_read(&file, read_buffer, remaining_chars_len, &bytes_read);
+
+                    if (result == FAT32_OK && bytes_read > 0)
+                    {
+                        read_buffer[bytes_read] = '\0';
+                        printf("\033[2J\033[H");
+                        printf("%s", read_buffer);
+                        sleep_ms(5000);
+                    }
+                }
+                else
+                {
+                    fat32_seek(&file, header[x].offset + current_char_index);
+
+                    size_t bytes_read;
+                    char read_buffer[SCREEN_AREA + 1];
+                    fat32_error_t result = fat32_read(&file, read_buffer, SCREEN_AREA, &bytes_read);
+
+                    if (result == FAT32_OK && bytes_read > 0)
+                    {
+                        read_buffer[bytes_read] = '\0';
+                        printf("\033[2J\033[H");
+                        printf("%s", read_buffer);
+                        sleep_ms(5000);
+                    }
+                }
+
+                current_char_index += SCREEN_AREA;
+            }
+        }
+    }
 
     fat32_close(&file);
 
